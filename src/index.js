a3fd7fff39b1ef78537ba83837bcdfeeb02f4c2d
marcosvinidss <151451317+marcosvinidss@users.noreply.github.com>
2025-05-13 07:44:58 -0300

    configurando o redux

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import {createStore} from 'redux'
+import{Provider} from 'react-redux'
+
 import App from './App';
+import Reducers from './Reducers';
+
+const store = createStore(Reducers);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
