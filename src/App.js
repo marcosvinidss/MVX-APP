d71043fabf96afea6be4a55e387912b58f02d44a
marcosvinidss <151451317+marcosvinidss@users.noreply.github.com>
2025-05-13 17:14:13 -0300

    Configurações gerais de CSS do projeto

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+
+import { Template } from './components/MainComponents';
+import Header from './components/partials/Header';
+import Footer from './components/partials/Footer';
+
+import "./App.css"
+
+
+import Routes from './Routes';
 
 const Page = (props) => {
   return(
-    <div>Opa, funcionando...</div>
+    <BrowserRouter>
+
+        <Template>
+            <Header />
+
+
+
+              <Routes />
+
+
+            <Footer />
+        </Template>
+    </BrowserRouter>
   )
 }
 
@@ -16,7 +38,7 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
   return{
-    
+
   }
 }
 
